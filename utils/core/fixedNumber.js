630833a800ece10f5387bc78f196ca69a4f6bd3f
china-521 <3052236335@qq.com>
2023-12-02 18:46:41 +0800

    update：优化 fixedNumber 固定位数方法

--- utils/core/fixedNumber.js
+++ utils/core/fixedNumber.js
@@ -1,17 +1,17 @@
 /**
  * 固定位数（位数不够，前面补充自定义数字）
- *  @param {Number} num：被操作数
- *  @param {Number} n： 固定的总位数
- *  @param {Number} num1：位数不足时要补充的数
+ *  @param {Number} num 被操作数
+ *  @param {Number} n  固定的总位数
+ *  @param {Number} fillNum 位数不足时要补充的数
  *  @returns {String}
  */
 
 import { stringify } from "./stringify";
 
-export function fixedNumber(num,n,num1){
+export function fixedNumber(num,n,fillNum){
 	if(stringify(num).split('').length > n){
 		return stringify(num);
 	}
-	num1 = num1 || 0;
-	return (Array(n).join(num1) + num).slice(-n);
+	fillNum = fillNum || 0;
+	return (Array(n).join(fillNum) + num).slice(-n);
 }
